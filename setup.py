12bd230c1c29002953ebe720b71736c7ef9ce0f1
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-06-07 18:20:15 +1000

    Updated version to 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import platform
 from distutils.core import setup, Command
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Taylor "Nekroze" Lawson'
 __email__ = 'nekroze@eturnilnetwork.com'
 SOURCE = 'librarian'
